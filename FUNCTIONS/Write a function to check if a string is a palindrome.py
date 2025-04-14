1b53d9bfd728390b9f2611b13a72bf94de344668
MuhammadAbdullah80042 <Abdullahhd04@gmail.com>
2025-04-14 23:25:12 +0500

    Update Write a function to check if a string is a palindrome.py

--- FUNCTIONS/Write a function to check if a string is a palindrome.py
+++ FUNCTIONS/Write a function to check if a string is a palindrome.py
@@ -3,9 +3,3 @@
 def is_palindrome(s):
     return s == s[::-1]
 
-# Q8: Write a function that accepts a dictionary of student names and marks, then prints students who scored above 80.
-
-def print_top_students(student_marks):
-    for name, marks in student_marks.items():
-        if marks > 80:
-            print("Student:", name, "Marks:", marks)
